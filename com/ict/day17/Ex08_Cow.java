240b5c529c781ff29c62589e1e8f3bc5491de0b2
LEEDAVID <seouldavid98@gmail.com>
2024-08-21 14:43:38 +0900

    update

---
+++ com/ict/day17/Ex08_Cow.java
@@ -0,0 +1,18 @@
+package com.ict.day17;
+
+public class Ex08_Cow implements Runnable {
+	public void start() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println("야옹~ : " + Thread.currentThread().getName());
+		}
+	}
+
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println("야옹~ : " + Thread.currentThread().getName());
+
+		}
+	}
+}
---
+++ com/ict/day17/Ex08_Main.java
@@ -0,0 +1,7 @@
+package com.ict.day17;
+
+public class Ex08_Main {
+	public static void main(String[] args) {
+		
+	}
+}
